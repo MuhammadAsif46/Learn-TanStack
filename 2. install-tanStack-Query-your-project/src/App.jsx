86320d53185e3fd6d179acc55f1437316e3d3254
MuhammadAsif46 <asifahmed32c@gmail.com>
2025-01-22 16:41:45 +0500

    add complete project setup with routing using react-router-dom

--- 2. install-tanStack-Query-your-project/src/App.jsx
+++ 2. install-tanStack-Query-your-project/src/App.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import React from "react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import MainLayout from "./components/Layouts/MainLayout";
+import Home from "./pages/Home";
+import FetchOld from "./pages/FetchOld";
+import FetchRQ from "./pages/FetchRQ";
 
 const App = () => {
-  return (
-    <h1>TanStack Query</h1>
-  )
-}
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <MainLayout />,
+      children: [
+        {
+          path: "/",
+          element: <Home />,
+        },
+        {
+          path: "/old",
+          element: <FetchOld />,
+        },
+        {
+          path: "/new",
+          element: <FetchRQ />,
+        },
+      ],
+    },
+  ]);
 
-export default App
+  return <RouterProvider router={router}></RouterProvider>
+};
+
+export default App;
