ff947a9ec988c42abe0d9796a6d6e8a63cb515ad
MuhammadAsif46 <asifahmed32c@gmail.com>
2025-01-28 22:39:25 +0500

    fetch api with react query its to simple

--- 3. adding-queryClient-and-queryClientProvider/src/pages/FetchRQ.jsx
+++ 3. adding-queryClient-and-queryClientProvider/src/pages/FetchRQ.jsx
@@ -1,9 +1,40 @@
-import React from 'react'
+import React from "react";
+import { fetchPosts } from "../api";
+
+import {useQuery} from "@tanstack/react-query"
 
 const FetchRQ = () => {
-  return (
-    <div> Hello FetchRQ</div>
-  )
-}
+  // 1st ways
+  // const getData = async () => {
+  //   const res = await fetchPosts();
+  //   console.log("res-->", res);
+  //   return res;
+  // };
 
-export default FetchRQ
+// only React query user
+  const {data} = useQuery({
+    queryKey: [ 'posts'], // work in useState
+    queryFn: fetchPosts,     // work in useEffect
+  })
+
+
+  return (
+    <div>
+      <h1>Fetch Data New Version</h1>
+      <ul className="section-accordion">
+        {data?.length > 0 ? (
+          data?.map((post) => (
+            <li key={post.id}>
+              <p>{post.title}</p>
+              <p>{post.body}</p>
+            </li>
+          ))
+        ) : (
+          <h1>Loading....</h1>
+        )}
+      </ul>
+    </div>
+  );
+};
+
+export default FetchRQ;
